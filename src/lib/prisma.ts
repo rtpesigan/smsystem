dd1d183504fe61e540d640edb5a5fb1c5b16a222
rtpesigan <rolandpesigan@gmail.com>
2026-03-11 13:20:28 +0800

    Fix Prisma log type casting

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,7 @@ const prismaLog = process.env.PRISMA_LOG_QUERIES === "true" ? ["query", "error"]
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: prismaLog,
+    log: prismaLog as ("query" | "info" | "warn" | "error")[],
   })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
